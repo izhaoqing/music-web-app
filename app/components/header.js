50ae12304130377aa90f2e1cd39ca27bd98d7ae8
chingchao <zhaoq0104@163.com>
2017-12-15 23:32:30 +0800

    back btn

--- app/components/header.js
+++ app/components/header.js
@@ -3,10 +3,13 @@ import './header.less';
 import { Link } from 'react-router';
 
 class Header extends Component {
+    handleBack() {
+        window.history.back();
+    }
     render() {
         return(
             <div className="components-header">
-                { this.props.left === 'left' ? (<span className='pull-left iconfont icon-fanhui' style={{fontSize: '1.1rem'}}></span>) : ''}
+                { this.props.left === 'left' ? (<span className='pull-left back iconfont icon-fanhui' style={{fontSize: '1.1rem'}} onClick={this.handleBack}></span>) : ''}
                 {/*<img width="40" src="../../static/images/logo.png" alt="" className="-col-auto" />*/}
                 <h1 className="caption">{this.props.title}</h1>
                 { this.props.right === 'list' ?
