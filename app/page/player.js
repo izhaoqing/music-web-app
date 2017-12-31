91a10f97cde36657f9330c5cd7053af09d286ab2
chingchao <zhaoq0104@163.com>
2017-12-31 15:11:59 +0800

    播放暂停状态

--- app/page/player.js
+++ app/page/player.js
@@ -30,11 +30,10 @@ export default class Player extends Component {
             }
         });
     }
-    componentWillReceiveProps () {
+    componentWillReceiveProps (next) {
         this.setState({
-            isPlay: this.props.isPlay
+            isPlay: next.isPlay
         });
-        console.log(this.props.isPlay);
     }
     componentWillUnmount() {
         $('#player').unbind($.jPlayer.event.timeupdate);
@@ -93,10 +92,10 @@ export default class Player extends Component {
                 </div>
                 <div className="controll-wrapper">
                     <h2 className="music-title">{this.props.currentMusicItem.name}</h2>
-                    <h3 className="music-artist">{this.props.currentMusicItem.singer}</h3>
+                    <h3 className="music-artist">{this.props.currentMusicItem.singer}{this.props.isPlay + ''}</h3>
                     <div className="flex" style={{flexDirection: 'row'}}>
                         <div className="volume-container flex">
-                            <i className="icon-volume" style={{marginRight: '.3rem'}}></i>
+                            <i className="icon-volume" style={{marginRight: '.3rem'}}> </i>
                             <div className="volume-wrapper">
                                 <Progress
                                     progress={this.state.volume}
@@ -118,23 +117,19 @@ export default class Player extends Component {
                     </div>
                     <div className="flex" style={{flexDirection: 'row', justifyContent: 'space-between', position: 'relative', alignItems:'center', height:'2rem'}}>
                         <div className='w100p flex set-icon'>
-                            <i className="iconfont icon-fanhui1 prev"
-                               onClick={this.prevMusic}
-                            ></i>
+                            <i className="iconfont icon-fanhui1 prev" onClick={this.prevMusic}/>
                             <i className={`iconfont change ${!this.state.isPlay ? 'icon-bofang pause' : 'icon-zanting play'}`}
-                               onClick={this.play.bind(this)}
-                            ></i>
-                            <i className="next iconfont icon-fanhui1-copy" onClick={this.nextMusic}></i>
+                               onClick={this.play.bind(this)} />
+                            <i className="next iconfont icon-fanhui1-copy" onClick={this.nextMusic}/>
                         </div>
                         <div className="set-repeat-icon">
                             <i className={`iconfont repeat ${this.state.repeatOnce? 'icon-danquxunhuan' : 'icon-xunhuan'}`}
-                               onClick={this.changeRepeat.bind(this)}
-                            ></i>
+                               onClick={this.changeRepeat.bind(this)} />
                         </div>
                     </div>
                 </div>
             </div>
-                <div className='filter wh bg-pic' style={{backgroundImage: `url(${this.props.currentMusicItem.image})`}}></div>
+                <div className='filter wh bg-pic' style={{backgroundImage: `url(${this.props.currentMusicItem.image})`}}> </div>
             </div>
         );
     }
